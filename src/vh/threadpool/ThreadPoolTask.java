af6c387ba4b3e3c24a46c4b79f939b9ea427a7e5
anehing <aneing@gmai.com>
2014-11-27 17:25:35 +0800

    完善测试

--- src/vh/threadpool/ThreadPoolTask.java
+++ src/vh/threadpool/ThreadPoolTask.java
@@ -11,6 +11,13 @@ public class ThreadPoolTask implements Runnable, Serializable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		System.out.println("开始执行任务前：" + attachData);  
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			System.out.println("等时间太长了"); 
+		}
 		System.out.println("开始执行任务：" + attachData);  
         
         attachData = null;  
