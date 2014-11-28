fdef0270befb3841119864e33b5d88556867e2c3
anehing <aneing@gmai.com>
2014-11-28 13:58:16 +0800

    Executor -->Executors;Thread-->ThreadFactory

---
+++ src/vh/threadfactory/DefaultThreadFactory.java
@@ -0,0 +1,33 @@
+package vh.threadfactory;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class DefaultThreadFactory implements ThreadFactory {
+
+	public static final AtomicInteger poolNumber =new AtomicInteger(1);
+	final ThreadGroup group;
+	final AtomicInteger threadNumber =  new AtomicInteger(1);
+	final String namePrefix;
+	
+	public DefaultThreadFactory() {
+		// TODO Auto-generated constructor stub
+		SecurityManager s =System.getSecurityManager();
+		group = (s!=null)? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+		
+		namePrefix = "pool-"+ poolNumber.getAndIncrement() +"-thread-";
+	}
+	@Override
+	public Thread newThread(Runnable r) {
+		// TODO Auto-generated method stub
+		Thread thread = new Thread(group,r,namePrefix+threadNumber.getAndIncrement(),0);
+		
+		if(thread.isDaemon()) thread.setDaemon(false);
+		if(thread.getPriority()!=Thread.NORM_PRIORITY) thread.setPriority(Thread.NORM_PRIORITY);
+		
+		return thread;
+	}
+	
+	
+
+}
---
+++ src/vh/threadfactory/ThreadFactorytest.java
@@ -0,0 +1,44 @@
+package vh.threadfactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+
+class Task implements Runnable{
+	int taskId;
+	public Task (int taskId) {
+		this.taskId = taskId;		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println(Thread.currentThread().getName()+"--taskId:"+taskId);
+	}
+	
+}
+class DaemonThreadFactory implements ThreadFactory{
+
+	@Override
+	public Thread newThread(Runnable r) {
+		// TODO Auto-generated method stub
+		Thread thread = new Thread();
+		thread.setDaemon(true);
+		return thread;
+	}
+	
+}
+public class ThreadFactorytest {
+
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stubq
+		ExecutorService es = Executors.newFixedThreadPool(3,new DefaultThreadFactory());	
+		for(int i=0;i<10;i++){
+			Task task = new Task(i);
+			es.submit(task);
+		}
+		es.shutdown();
+		System.out.println("over");
+		
+	}
+}
