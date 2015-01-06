f37681e68fb87f87931f99c5da952d2df39fda15
anehing <aneing@gmai.com>
2015-01-06 16:28:32 +0800

    local thread variable

---
+++ src/vh/local/thread/Main.java
@@ -0,0 +1,22 @@
+package vh.local.thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//UnsafeTask task = new UnsafeTask();
+		SafeTask task = new SafeTask();
+		for(int i = 0;i<10;i++){
+		  Thread thread = new Thread(task);
+		  thread.start();
+		  try{
+			  TimeUnit.SECONDS.sleep(2);
+		  } catch(InterruptedException e){
+			  e.printStackTrace();
+		  }
+		}
+	}
+
+}
---
+++ src/vh/local/thread/SafeTask.java
@@ -0,0 +1,26 @@
+package vh.local.thread;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class SafeTask implements Runnable {
+  
+//   private static ThreadLocal<Date> startDate = new ThreadLocal<Date>(){
+//	  protected Date initialValue(){
+//		  return new Date();
+//	  }
+//   };
+   private static ThreadLocal<Date> startDate = ThreadLocal.withInitial(Date::new);
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.printf("Starting Thread: %s : %s\n",Thread.currentThread().getId(),startDate.get());
+		try {
+			TimeUnit.SECONDS.sleep((int)Math.rint(Math.random()*10));
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		System.out.printf("Thread Finished: %s : %s\n",Thread.currentThread().getId(),startDate.get());
+	}
+}
