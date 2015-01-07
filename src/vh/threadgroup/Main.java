27f04c0ad21ea717ce780dea692d824ca8e80228
anehing <aneing@gmai.com>
2015-01-07 12:03:39 +0800

    threadGroup

--- src/vh/threadgroup/Main.java
+++ src/vh/threadgroup/Main.java
@@ -1,5 +1,8 @@
 package vh.threadgroup;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,6 +10,34 @@ public class Main {
 		ThreadGroup threadGroup = new ThreadGroup("Searcher");
 		Result result = new Result();
 		SearchTask searchTask = new SearchTask(result);
+		for (int i = 0; i <5 ; i++) {
+			Thread thread = new Thread(threadGroup,searchTask);
+			thread.start();
+			try{
+				TimeUnit.SECONDS.sleep(1);
+			}catch (InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+		System.out.println(threadGroup.activeCount() + " threads in thread group.");
+		System.out.printf("Number of threads : %d\n", threadGroup.activeCount());
+		System.out.printf("Information about the Thread Group\n");
+		threadGroup.list();
+		Thread[] threads = new Thread[threadGroup.activeCount()];
+		threadGroup.enumerate(threads);
+		for (int i = 0; i <threadGroup.activeCount() ; i++) {
+			System.out.printf("Thread %s: %s\n",threads[i].getName(),threads[i].getState());
+		}
+		waitFinish(threadGroup);
+		threadGroup.interrupt();
 	}
-
+    private static void waitFinish(ThreadGroup threadGroup){
+		while(threadGroup.activeCount() > 9){
+			try{
+				TimeUnit.SECONDS.sleep(1);
+			}catch (InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+	}
 }
